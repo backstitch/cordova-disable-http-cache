a36c4e2cb0bfebcad7276bb395a5c81a0fa178e7
nate-backstitch <nate@backstitch.io>
2021-08-11 13:25:37 -0500

    Some fixes

--- src/ios/DisableHttpCachePlugin.h
+++ src/ios/DisableHttpCachePlugin.h
@@ -1,5 +1,6 @@
 #import <Cordova/CDV.h>
 #import <WebKit/WebKit.h>
+#import <WebKit/WKWebsiteDataStore.h>
 
 @interface DisableHttpCachePlugin :CDVPlugin
 @end
--- src/ios/DisableHttpCachePlugin.m
+++ src/ios/DisableHttpCachePlugin.m
@@ -7,7 +7,7 @@
   NSURLCache *URLCache = [[NSURLCache alloc] initWithMemoryCapacity:0 diskCapacity:0 diskPath:nil];
   [NSURLCache setSharedURLCache:URLCache];
 
-  // We need this so that we can utilize the onPause hook
+  // We need this to enable the onPause hook
   [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onPause) name:UIApplicationDidEnterBackgroundNotification object:nil];
 }
 
@@ -15,46 +15,47 @@
   return self;
 }
 
-- (void)onAppTerminate {
+- (void)onAppTerminate
+{
   NSLog(@"DisableHttpCachePlugin: onAppTerminate");
   NSURLCache *URLCache = [[NSURLCache alloc] initWithMemoryCapacity:0 diskCapacity:0 diskPath:nil];
   [NSURLCache setSharedURLCache:URLCache];
   [[NSURLCache sharedURLCache] removeAllCachedResponses];
     NSSet *dataTypes = [NSSet setWithArray:@[WKWebsiteDataTypeDiskCache,
-                        WKWebsiteDataTypeMemoryCache,
-                        WKWebsiteDataTypeDiskCache,
-                        WKWebsiteDataTypeOfflineWebApplicationCache,
-                        WKWebsiteDataTypeMemoryCache,
-                        WKWebsiteDataTypeLocalStorage,
-                        WKWebsiteDataTypeCookies,
-                        WKWebsiteDataTypeSessionStorage,
-                        WKWebsiteDataTypeIndexedDBDatabases,
-                        WKWebsiteDataTypeWebSQLDatabases
+                                             WKWebsiteDataTypeOfflineWebApplicationCache,
+                                             WKWebsiteDataTypeMemoryCache,
+                                             WKWebsiteDataTypeLocalStorage,
+                                             WKWebsiteDataTypeCookies,
+                                             WKWebsiteDataTypeSessionStorage,
+                                             WKWebsiteDataTypeIndexedDBDatabases,
+                                             WKWebsiteDataTypeWebSQLDatabases
     ]];
     [[WKWebsiteDataStore defaultDataStore] removeDataOfTypes:dataTypes
                                              modifiedSince:[NSDate dateWithTimeIntervalSince1970:0]
-                                         completionHandler:^{}
-                                         ];
+                                         completionHandler:^{
+          }];
 }
 
-- (void)onPause {
-  NSLog(@"DisableHttpCachePlugin: onPause");
-  NSURLCache *URLCache = [[NSURLCache alloc] initWithMemoryCapacity:0 diskCapacity:0 diskPath:nil];
-  [NSURLCache setSharedURLCache:URLCache];
-  [[NSURLCache sharedURLCache] removeAllCachedResponses];
-    NSSet *dataTypes = [NSSet setWithArray:@[WKWebsiteDataTypeDiskCache,
-                        WKWebsiteDataTypeMemoryCache,
-                        WKWebsiteDataTypeDiskCache,
-                        WKWebsiteDataTypeOfflineWebApplicationCache,
-                        WKWebsiteDataTypeMemoryCache,
-                        WKWebsiteDataTypeSessionStorage,
-                        WKWebsiteDataTypeIndexedDBDatabases,
-                        WKWebsiteDataTypeWebSQLDatabases
-    ]];
-    [[WKWebsiteDataStore defaultDataStore] removeDataOfTypes:dataTypes
-                                             modifiedSince:[NSDate dateWithTimeIntervalSince1970:0]
-                                         completionHandler:^{}
-                                         ];
+
+- (void)onPause
+{
+    NSLog(@"DisableHttpCachePlugin: onPause");
+    NSURLCache *URLCache = [[NSURLCache alloc] initWithMemoryCapacity:0 diskCapacity:0 diskPath:nil];
+    [NSURLCache setSharedURLCache:URLCache];
+    [[NSURLCache sharedURLCache] removeAllCachedResponses];
+      NSSet *dataTypes = [NSSet setWithArray:@[WKWebsiteDataTypeDiskCache,
+                                               WKWebsiteDataTypeOfflineWebApplicationCache,
+                                               WKWebsiteDataTypeMemoryCache,
+                                               WKWebsiteDataTypeLocalStorage,
+                                               WKWebsiteDataTypeCookies,
+                                               WKWebsiteDataTypeSessionStorage,
+                                               WKWebsiteDataTypeIndexedDBDatabases,
+                                               WKWebsiteDataTypeWebSQLDatabases
+      ]];
+      [[WKWebsiteDataStore defaultDataStore] removeDataOfTypes:dataTypes
+                                               modifiedSince:[NSDate dateWithTimeIntervalSince1970:0]
+                                           completionHandler:^{
+            }];
 }
 
 @end
