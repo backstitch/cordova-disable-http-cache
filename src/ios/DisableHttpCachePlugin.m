d2726374c4d98acfed3f26ee351aae7b85cec557
nate-backstitch <nate@backstitch.io>
2021-08-11 12:35:21 -0500

    Fixed ios plugin

--- src/ios/DisableHttpCachePlugin.m
+++ src/ios/DisableHttpCachePlugin.m
@@ -6,34 +6,55 @@
   NSLog(@"DisableHttpCachePlugin: initialize");
   NSURLCache *URLCache = [[NSURLCache alloc] initWithMemoryCapacity:0 diskCapacity:0 diskPath:nil];
   [NSURLCache setSharedURLCache:URLCache];
+
+  // We need this so that we can utilize the onPause hook
+  [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onPause) name:UIApplicationDidEnterBackgroundNotification object:nil];
 }
 
 - (DisableHttpCachePlugin*)initWithWebView:(UIWebView*)theWebView {
   return self;
 }
 
-- (void)onAppTerminate
-{
+- (void)onAppTerminate {
   NSLog(@"DisableHttpCachePlugin: onAppTerminate");
   NSURLCache *URLCache = [[NSURLCache alloc] initWithMemoryCapacity:0 diskCapacity:0 diskPath:nil];
   [NSURLCache setSharedURLCache:URLCache];
   [[NSURLCache sharedURLCache] removeAllCachedResponses];
+    NSSet *dataTypes = [NSSet setWithArray:@[WKWebsiteDataTypeDiskCache,
+                        WKWebsiteDataTypeMemoryCache,
+                        WKWebsiteDataTypeDiskCache,
+                        WKWebsiteDataTypeOfflineWebApplicationCache,
+                        WKWebsiteDataTypeMemoryCache,
+                        WKWebsiteDataTypeLocalStorage,
+                        WKWebsiteDataTypeCookies,
+                        WKWebsiteDataTypeSessionStorage,
+                        WKWebsiteDataTypeIndexedDBDatabases,
+                        WKWebsiteDataTypeWebSQLDatabases
+    ]];
+    [[WKWebsiteDataStore defaultDataStore] removeDataOfTypes:dataTypes
+                                             modifiedSince:[NSDate dateWithTimeIntervalSince1970:0]
+                                         completionHandler:^{}
+                                         ];
 }
 
-- (void)onReset
-{
-  NSLog(@"DisableHttpCachePlugin: onReset");
+- (void)onPause {
+  NSLog(@"DisableHttpCachePlugin: onPause");
   NSURLCache *URLCache = [[NSURLCache alloc] initWithMemoryCapacity:0 diskCapacity:0 diskPath:nil];
   [NSURLCache setSharedURLCache:URLCache];
   [[NSURLCache sharedURLCache] removeAllCachedResponses];
-}
-
-- (void)dispose
-{
-  NSLog(@"DisableHttpCachePlugin: dispose");
-  NSURLCache *URLCache = [[NSURLCache alloc] initWithMemoryCapacity:0 diskCapacity:0 diskPath:nil];
-  [NSURLCache setSharedURLCache:URLCache];
-  [[NSURLCache sharedURLCache] removeAllCachedResponses];
+    NSSet *dataTypes = [NSSet setWithArray:@[WKWebsiteDataTypeDiskCache,
+                        WKWebsiteDataTypeMemoryCache,
+                        WKWebsiteDataTypeDiskCache,
+                        WKWebsiteDataTypeOfflineWebApplicationCache,
+                        WKWebsiteDataTypeMemoryCache,
+                        WKWebsiteDataTypeSessionStorage,
+                        WKWebsiteDataTypeIndexedDBDatabases,
+                        WKWebsiteDataTypeWebSQLDatabases
+    ]];
+    [[WKWebsiteDataStore defaultDataStore] removeDataOfTypes:dataTypes
+                                             modifiedSince:[NSDate dateWithTimeIntervalSince1970:0]
+                                         completionHandler:^{}
+                                         ];
 }
 
 @end
