36b8c7b3b051d3c71d9fdf5cb6189bc3b607f947
nate-backstitch <nate@backstitch.io>
2021-08-11 09:47:53 -0500

    Added cleanup to onreset and dispose

--- src/ios/DisableHttpCachePlugin.m
+++ src/ios/DisableHttpCachePlugin.m
@@ -20,4 +20,20 @@
   [[NSURLCache sharedURLCache] removeAllCachedResponses];
 }
 
+- (void)onReset
+{
+  NSLog(@"DisableHttpCachePlugin: onReset");
+  NSURLCache *URLCache = [[NSURLCache alloc] initWithMemoryCapacity:0 diskCapacity:0 diskPath:nil];
+  [NSURLCache setSharedURLCache:URLCache];
+  [[NSURLCache sharedURLCache] removeAllCachedResponses];
+}
+
+- (void)dispose
+{
+  NSLog(@"DisableHttpCachePlugin: dispose");
+  NSURLCache *URLCache = [[NSURLCache alloc] initWithMemoryCapacity:0 diskCapacity:0 diskPath:nil];
+  [NSURLCache setSharedURLCache:URLCache];
+  [[NSURLCache sharedURLCache] removeAllCachedResponses];
+}
+
 @end
