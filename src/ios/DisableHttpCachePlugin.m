44cf3645515b2c95aeba0e9f696bad97c0da74ba
nate-backstitch <nate@backstitch.io>
2021-08-16 13:31:39 -0500

    Deleting network cache without logging out user

--- src/ios/DisableHttpCachePlugin.m
+++ src/ios/DisableHttpCachePlugin.m
@@ -24,7 +24,6 @@
     NSSet *dataTypes = [NSSet setWithArray:@[WKWebsiteDataTypeDiskCache,
                                              WKWebsiteDataTypeOfflineWebApplicationCache,
                                              WKWebsiteDataTypeMemoryCache,
-                                             WKWebsiteDataTypeLocalStorage,
                                              WKWebsiteDataTypeCookies,
                                              WKWebsiteDataTypeSessionStorage,
                                              WKWebsiteDataTypeIndexedDBDatabases,
@@ -34,6 +33,23 @@
                                              modifiedSince:[NSDate dateWithTimeIntervalSince1970:0]
                                          completionHandler:^{
           }];
+
+    // ONLY delete the files in the NetworkCache folder for WebKit
+    // This replaces the WKWebsiteDataTypeLocalStorage flag since that also logs out the user
+    NSFileManager *fm = [NSFileManager defaultManager];
+
+    // NSHomeDirectory() is something like /var/mobile/Containers/Data/Application/42DEC198-3D99-4A74-B21D-A87F001512BA
+    NSString *directory = [NSHomeDirectory() stringByAppendingPathComponent:@"/Library/Caches/WebKit/NetworkCache"];
+    NSError *error = nil;
+    for (NSString *file in [fm contentsOfDirectoryAtPath:directory error:&error]) {
+        NSLog(@"Directory: %@; File: %@", directory, file);
+        BOOL success = [fm removeItemAtPath:[NSString stringWithFormat:@"%@", directory] error:&error];
+        if (!success || error) {
+            NSLog (@"Remove failed");
+        }else {
+            NSLog (@"Remove successful");
+        }
+    }
 }
 
 
@@ -46,7 +62,6 @@
       NSSet *dataTypes = [NSSet setWithArray:@[WKWebsiteDataTypeDiskCache,
                                                WKWebsiteDataTypeOfflineWebApplicationCache,
                                                WKWebsiteDataTypeMemoryCache,
-                                               WKWebsiteDataTypeLocalStorage,
                                                WKWebsiteDataTypeCookies,
                                                WKWebsiteDataTypeSessionStorage,
                                                WKWebsiteDataTypeIndexedDBDatabases,
@@ -56,6 +71,23 @@
                                                modifiedSince:[NSDate dateWithTimeIntervalSince1970:0]
                                            completionHandler:^{
             }];
+    
+    // ONLY delete the files in the NetworkCache folder for WebKit
+    // This replaces the WKWebsiteDataTypeLocalStorage flag since that also logs out the user
+    NSFileManager *fm = [NSFileManager defaultManager];
+
+    // NSHomeDirectory() is something like /var/mobile/Containers/Data/Application/42DEC198-3D99-4A74-B21D-A87F001512BA
+    NSString *directory = [NSHomeDirectory() stringByAppendingPathComponent:@"/Library/Caches/WebKit/NetworkCache"];
+    NSError *error = nil;
+    for (NSString *file in [fm contentsOfDirectoryAtPath:directory error:&error]) {
+        NSLog(@"Directory: %@; File: %@", directory, file);
+        BOOL success = [fm removeItemAtPath:[NSString stringWithFormat:@"%@", directory] error:&error];
+        if (!success || error) {
+            NSLog (@"Remove failed");
+        }else {
+            NSLog (@"Remove successful");
+        }
+    }
 }
 
 @end
